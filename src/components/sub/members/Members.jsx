f9f7cfe0e97f4558f844d68b3ef35755b3741386
LeeSeongWon05 <lsw050103@gmail.com>
2023-10-12 11:49:54 +0900

    저장

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,7 +4,40 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<p>회원가입 페이지입니다.</p>
+			<div class='inner'>
+				<h1 className='title'>Our Team</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio!</p>
+				<div class='wrap'>
+					<article>
+						<img src='img/member1.jpg' alt='' class='pic' />
+						<div class='txtBox'>
+							<h2>Lorem ipsum dolor sit.</h2>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
+						</div>
+					</article>
+					<article>
+						<img src='img/member2.jpg' alt='' class='pic' />
+						<div class='txtBox'>
+							<h2>Lorem ipsum dolor sit.</h2>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
+						</div>
+					</article>
+					<article>
+						<img src='img/member3.jpg' alt='' class='pic' />
+						<div class='txtBox'>
+							<h2>Lorem ipsum dolor sit.</h2>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
+						</div>
+					</article>
+					<article>
+						<img src='img/member4.jpg' alt='' class='pic' />
+						<div class='txtBox'>
+							<h2>Lorem ipsum dolor sit.</h2>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
+						</div>
+					</article>
+				</div>
+			</div>
 		</Layout>
 	);
 }
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -11,7 +11,7 @@ export default function Youtube() {
 	const fetchYoutube = async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const pid = 'PL9I0cGMkw7P6QXU1F9HI0JE370vyxQsV3';
 		const num = 5;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
@@ -34,11 +34,17 @@ export default function Youtube() {
 
 					return (
 						<article key={idx}>
-							<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-							<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
-							<span>{date.split('T')[0].split('-').join('.')}</span>
+							<div className='titBox'>
+								<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
+							</div>
+
+							<div className='conBox'>
+								<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
+								<span>{date.split('T')[0].split('-').join('.')}</span>
+							</div>
+
 							<div
-								className='pic'
+								className='picBox'
 								onClick={() => {
 									setIndex(idx);
 									setIsModal(true);
