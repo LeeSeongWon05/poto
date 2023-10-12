ac4a0cd98d31667af934e61f1de03ffc05ff03e3
LeeSeongWon05 <lsw050103@gmail.com>
2023-10-12 12:35:44 +0900

    저장

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,40 +4,7 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<div class='inner'>
-				<h1 className='title'>Our Team</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio!</p>
-				<div class='wrap'>
-					<article>
-						<img src='img/member1.jpg' alt='' class='pic' />
-						<div class='txtBox'>
-							<h2>Lorem ipsum dolor sit.</h2>
-							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
-						</div>
-					</article>
-					<article>
-						<img src='img/member2.jpg' alt='' class='pic' />
-						<div class='txtBox'>
-							<h2>Lorem ipsum dolor sit.</h2>
-							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
-						</div>
-					</article>
-					<article>
-						<img src='img/member3.jpg' alt='' class='pic' />
-						<div class='txtBox'>
-							<h2>Lorem ipsum dolor sit.</h2>
-							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
-						</div>
-					</article>
-					<article>
-						<img src='img/member4.jpg' alt='' class='pic' />
-						<div class='txtBox'>
-							<h2>Lorem ipsum dolor sit.</h2>
-							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, odio.</p>
-						</div>
-					</article>
-				</div>
-			</div>
+			<p>회원가입 페이지입니다.</p>
 		</Layout>
 	);
 }
